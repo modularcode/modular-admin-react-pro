10eba915fda82b1e86dd1a4c5b6fc6190359de6e
Gevorg Harutyunyan <gevharut@gmail.com>
2019-07-28 03:12:56 +0400

    Updates

---
+++ precache-manifest.b47584d6f411fd9e53a3f007a0c00603.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0afbcfb77388eca000540e86dbd50ea2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83f51a34c72edcecb98c",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "65ebeeab10dcb44539f6",
+    "url": "/static/js/2.c2b57fd7.chunk.js"
+  },
+  {
+    "revision": "83f51a34c72edcecb98c",
+    "url": "/static/js/main.58328058.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
