510e6c3364f5c1397d9633fb93c5509d1354ca3b
Gevorg Harutyunyan <gevharut@gmail.com>
2019-07-27 05:17:52 +0400

    Implement header interaction buttons

--- src/Dashboard/_common/Header/Header.tsx
+++ src/Dashboard/_common/Header/Header.tsx
@@ -3,12 +3,19 @@ import clsx from 'clsx'
 
 import { makeStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
+import Avatar from '@material-ui/core/Avatar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import IconButton from '@material-ui/core/IconButton'
 import Badge from '@material-ui/core/Badge'
-import MenuIcon from '@material-ui/icons/Menu'
+// import Menu from '@material-ui/core/Menu'
+// import MenuItem from '@material-ui/core/MenuItem'
+import IconMenu from '@material-ui/icons/Menu'
+import IconSearch from '@material-ui/icons/Search'
+import IconArrowDropDown from '@material-ui/icons/ArrowDropDown'
 import NotificationsIcon from '@material-ui/icons/Notifications'
+
+// import HeaderSearch from './HeaderSearch'
 
 interface HeaderProps {
   onToggle?: (event: React.MouseEvent<HTMLButtonElement>) => void
@@ -18,27 +25,38 @@ const Header = ({ onToggle }: HeaderProps) => {
   const classes = useStyles()
 
   return (
-    // <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)}>
-    <AppBar position="absolute" className={clsx(classes.appBar)}>
+    <AppBar position="absolute">
       <Toolbar className={classes.toolbar}>
         <IconButton
           edge="start"
           color="inherit"
-          aria-label="Open drawer"
+          aria-label="Toggle sidebar"
           onClick={onToggle}
           className={clsx(classes.menuButton)}
         >
-          <MenuIcon />
+          <IconMenu />
         </IconButton>
         <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
           Dashboard
         </Typography>
-        <IconButton color="inherit">
+        <IconButton edge="start" color="inherit" aria-label="Search" className={classes.searchButton}>
+          <IconSearch />
+        </IconButton>
+        {/* <HeaderSearch /> */}
+        <IconButton edge="start" color="inherit" aria-label="Search" className={classes.notificationsButton}>
           <Badge badgeContent={4} color="secondary">
             <NotificationsIcon />
           </Badge>
         </IconButton>
-        <IconButton color="inherit"></IconButton>
+        <IconButton edge="start" color="inherit" aria-label="Search" className={classes.profileButton}>
+          <Avatar
+            className={classes.profileAvatar}
+            alt="John Doe"
+            src="https://avatars3.githubusercontent.com/u/3959008?v=3&s=40"
+          />
+          <span className={classes.profileName}>John Doe</span>
+          <IconArrowDropDown />
+        </IconButton>
       </Toolbar>
     </AppBar>
   )
@@ -48,25 +66,31 @@ const useStyles = makeStyles(theme => ({
   toolbar: {
     paddingRight: 24, // keep right padding when drawer closed
   },
-  toolbarIcon: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: '0 8px',
-    ...theme.mixins.toolbar,
-  },
-  appBar: {
-    // zIndex: theme.zIndex.drawer + 1,
-    // transition: theme.transitions.create(['width', 'margin'], {
-    //   easing: theme.transitions.easing.sharp,
-    //   duration: theme.transitions.duration.leavingScreen,
-    // }),
-  },
   menuButton: {
-    marginRight: 36,
+    marginRight: 15,
   },
-  menuButtonHidden: {
-    display: 'none',
+  searchButton: {
+    marginRight: 20,
+  },
+  notificationsButton: {
+    marginRight: 23,
+  },
+  profileButton: {
+    borderRadius: '30px',
+    fontSize: '1.2rem',
+    padding: '8px',
+  },
+  profileAvatar: {
+    width: 35,
+    height: 35,
+    marginRight: 10,
+  },
+  profileName: {
+    fontWeight: 'bold',
+    marginRight: 5,
+    [theme.breakpoints.down('sm')]: {
+      display: 'none',
+    },
   },
   title: {
     flexGrow: 1,
---
+++ src/Dashboard/_common/Header/HeaderSearch.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const HeaderSearch = () => {
+  return <div></div>
+}
+
+export default HeaderSearch
