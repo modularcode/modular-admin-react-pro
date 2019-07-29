1472cc2523dce534e2879e2bc94f486346f8a2c3
Gevorg Harutyunyan <gevharut@gmail.com>
2019-07-29 11:04:30 +0400

    Remove Sidebar Dead Code

--- src/Dashboard/_common/TheSidebar/SidebarNav.tsx
+++ src/Dashboard/_common/TheSidebar/SidebarNav.tsx
@@ -1,18 +1,7 @@
-import React, { forwardRef } from 'react'
-import clsx from 'clsx'
-import PropTypes from 'prop-types'
+import React from 'react'
 import { makeStyles, createStyles } from '@material-ui/core/styles'
-
-import { NavLink, NavLinkProps } from 'react-router-dom'
 import List from '@material-ui/core/List'
 import ListSubheader from '@material-ui/core/ListSubheader'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemIcon from '@material-ui/core/ListItemIcon'
-import ListItemText from '@material-ui/core/ListItemText'
-import Divider from '@material-ui/core/Divider'
-import Collapse from '@material-ui/core/Collapse'
-
-import Tooltip from '@material-ui/core/Tooltip'
 
 import { SvgIconProps } from '@material-ui/core/SvgIcon'
 import IconDashboard from '@material-ui/icons/Dashboard'
@@ -21,12 +10,9 @@ import IconPeople from '@material-ui/icons/People'
 import IconBarChart from '@material-ui/icons/BarChart'
 import IconPersonalVideo from '@material-ui/icons/PersonalVideo'
 import IconLibraryBooks from '@material-ui/icons/LibraryBooks'
-import IconThumbUp from '@material-ui/icons/ThumbUp'
 import IconQuestionAnswer from '@material-ui/icons/QuestionAnswer'
-import IconBallot from '@material-ui/icons/Ballot'
 import IconStars from '@material-ui/icons/Stars'
 import IconNewReleases from '@material-ui/icons/NewReleases'
-import IconSpacer from '@material-ui/icons/FiberManualRecord'
 
 import { Theme } from '_theme'
 import SidebarNavListItem from './SidebarNavListItem'
@@ -39,6 +25,7 @@ interface SidebarNavListItem {
   name: string
   link?: string
   Icon?: React.ComponentType<SvgIconProps>
+  IconStyle?: object
 }
 
 interface SidebarNavListProps {
@@ -117,21 +104,33 @@ const sidebarNavListItemsMisc = [
     name: 'Why Modular?',
     link: '/demo/features',
     Icon: IconNewReleases,
+    IconStyle: {
+      color: '#95de3c',
+    },
   },
   {
     name: 'Docs',
     link: '/demo/docs',
     Icon: IconLibraryBooks,
+    IconStyle: {
+      color: '#f8cda9',
+    },
   },
   {
     name: 'Supporters',
     link: '/demo/supporters',
     Icon: IconStars,
+    IconStyle: {
+      color: '#e3b546',
+    },
   },
   {
     name: 'Discuss',
     link: '/demo/discuss',
     Icon: IconQuestionAnswer,
+    IconStyle: {
+      color: '#ccc',
+    },
   },
 ]
 
@@ -156,22 +155,14 @@ const SidebarNavListItems: React.FC<SidebarNavListProps> = (
 }
 
 const SidebarNav = (props: SidebarNavProps) => {
-  console.log('rerendered SidebarNav')
-
   const { isCollapsed } = props
-
   const classes = useStyles()
-  const [open, setOpen] = React.useState(false)
-
-  function handleClick() {
-    setOpen(!open)
-  }
 
   return (
     <div>
       <List className={classes.navList}>
         {!isCollapsed && (
-          <ListSubheader inset disableSticky={true} className={classes.navItem}>
+          <ListSubheader inset disableSticky={true}>
             Main Modules
           </ListSubheader>
         )}
@@ -180,7 +171,7 @@ const SidebarNav = (props: SidebarNavProps) => {
       </List>
       <List className={classes.navList}>
         {!isCollapsed && (
-          <ListSubheader inset disableSticky={true} className={classes.navItem}>
+          <ListSubheader inset disableSticky={true}>
             UI & Utils
           </ListSubheader>
         )}
@@ -194,79 +185,6 @@ const SidebarNav = (props: SidebarNavProps) => {
         )}
         <SidebarNavListItems isCollapsed={isCollapsed} items={sidebarNavListItemsMisc} />
       </List>
-
-      {/* <List className={classes.navList}>
-        {!isCollapsed && (
-          <ListSubheader inset disableSticky={true} className={classes.navItem}>
-            UI & Utils
-          </ListSubheader>
-        )}
-        <ListItem
-          component={ListItemLink}
-          button
-          to="/demo/components"
-          className={classes.navItem}
-        >
-          <ListItemIcon>
-            <IconPersonalVideo />
-          </ListItemIcon>
-          <ListItemText primary="UI Components" />
-        </ListItem>
-      </List>
-
-      <List className={classes.navList}>
-        {!isCollapsed && (
-          <ListSubheader inset disableSticky={true}>
-            Misc
-          </ListSubheader>
-        )}
-        <ListItem
-          component={ListItemLink}
-          to="/demo/features"
-          button
-          className={classes.navItem}
-        >
-          <ListItemIcon className={classes.iconFeatures}>
-            <IconNewReleases />
-          </ListItemIcon>
-          <ListItemText primary="Why Modular?" />
-        </ListItem>
-        <ListItem
-          component={ListItemLink}
-          to="/demo/docs"
-          button
-          className={classes.navItem}
-        >
-          <ListItemIcon className={classes.iconDocs}>
-            <IconLibraryBooks />
-          </ListItemIcon>
-          <ListItemText primary="Docs" />
-        </ListItem>
-
-        <ListItem
-          component={ListItemLink}
-          to="/demo/supporters"
-          button
-          className={classes.navItem}
-        >
-          <ListItemIcon className={classes.iconSponsors}>
-            <IconStars />
-          </ListItemIcon>
-          <ListItemText primary="Supporters" />
-        </ListItem>
-
-        <ListItem
-          component={ListItemLink}
-          to="/demo/discuss"
-          button
-          className={classes.navItem}
-        >
-          <ListItemIcon className={classes.iconDiscuss}>
-            <IconQuestionAnswer />
-          </ListItemIcon>
-          <ListItemText primary="Discuss" />
-        </ListItem>
-      </List> */}
     </div>
   )
 }
@@ -278,41 +196,6 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     navList: {
       width: theme.sidebar.width,
-    },
-    navItem: {
-      '&.active': {
-        // color: theme.palette.primary.main,
-        background: 'rgba(0, 0, 0, 0.08)',
-        '& .MuiListItemIcon-root': {
-          // display: 'none',
-          color: '#fff',
-          // color: theme.palette.primary.main,
-        },
-      },
-    },
-    navItemCollapsed: {
-      whiteSpace: 'nowrap',
-      flexWrap: 'nowrap',
-      width: theme.sidebar.widthCollapsed,
-    },
-    iconSpacer: {
-      fontSize: 13,
-      marginLeft: 6,
-    },
-    iconFeatures: {
-      color: '#95de3c',
-    },
-    iconDocs: {
-      color: '#f8cda9',
-    },
-    iconSupport: {
-      color: '#45d0ff',
-    },
-    iconSponsors: {
-      color: '#e3b546',
-    },
-    iconDiscuss: {
-      color: '#ccc',
     },
   }),
 )
